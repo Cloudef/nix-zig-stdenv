65aa8d2446570250591ae70f6fc1e09bf9e0b1cb
Jari Vetoniemi <mailroxas@gmail.com>
2022-11-02 11:02:53 +0900

    formatting

--- src/toolchain.nix
+++ src/toolchain.nix
@@ -37,33 +37,33 @@ let
     dontFixup = true;
 
     installPhase = ''
-    mkdir -p $out/bin $out/lib
-    for prog in ${llvm}/bin/*; do
-      ln -sf $prog $out/bin/${prefix}$(basename $prog)
-    done
+      mkdir -p $out/bin $out/lib
+      for prog in ${llvm}/bin/*; do
+        ln -sf $prog $out/bin/${prefix}$(basename $prog)
+      done
 
-    ln -s ${llvm}/bin/llvm-as $out/bin/${prefix}as
-    ln -s ${llvm}/bin/llvm-dwp $out/bin/${prefix}dwp
-    ln -s ${llvm}/bin/llvm-nm $out/bin/${prefix}nm
-    ln -s ${llvm}/bin/llvm-objcopy $out/bin/${prefix}objcopy
-    ln -s ${llvm}/bin/llvm-objdump $out/bin/${prefix}objdump
-    ln -s ${llvm}/bin/llvm-readelf $out/bin/${prefix}readelf
-    ln -s ${llvm}/bin/llvm-size $out/bin/${prefix}size
-    ln -s ${llvm}/bin/llvm-strip $out/bin/${prefix}strip
-    ln -s ${llvm}/bin/llvm-rc $out/bin/${prefix}windres
+      ln -s ${llvm}/bin/llvm-as $out/bin/${prefix}as
+      ln -s ${llvm}/bin/llvm-dwp $out/bin/${prefix}dwp
+      ln -s ${llvm}/bin/llvm-nm $out/bin/${prefix}nm
+      ln -s ${llvm}/bin/llvm-objcopy $out/bin/${prefix}objcopy
+      ln -s ${llvm}/bin/llvm-objdump $out/bin/${prefix}objdump
+      ln -s ${llvm}/bin/llvm-readelf $out/bin/${prefix}readelf
+      ln -s ${llvm}/bin/llvm-size $out/bin/${prefix}size
+      ln -s ${llvm}/bin/llvm-strip $out/bin/${prefix}strip
+      ln -s ${llvm}/bin/llvm-rc $out/bin/${prefix}windres
 
-    for f in ld ar ranlib dlltool lib; do
-      rm -f ${prefix}$f
-    done
+      for f in ld ar ranlib dlltool lib; do
+        rm -f ${prefix}$f
+      done
 
-    ln -s ${write-wrapper "cc"} $out/bin/clang
-    ln -s ${write-wrapper "c++"} $out/bin/clang++
-    ln -s ${write-wrapper "ld"} $out/bin/${prefix}ld
-    ln -s ${write-wrapper "ar"} $out/bin/${prefix}ar
-    ln -s ${write-wrapper "ranlib"} $out/bin/${prefix}ranlib
-    ln -s ${write-wrapper "dlltool"} $out/bin/${prefix}dlltool
-    ln -s ${write-wrapper "lib"} $out/bin/${prefix}lib
-  '';
+      ln -s ${write-cc-wrapper "cc"} $out/bin/clang
+      ln -s ${write-cc-wrapper "c++"} $out/bin/clang++
+      ln -s ${write-wrapper "ld"} $out/bin/${prefix}ld
+      ln -s ${write-wrapper "ar"} $out/bin/${prefix}ar
+      ln -s ${write-wrapper "ranlib"} $out/bin/${prefix}ranlib
+      ln -s ${write-wrapper "dlltool"} $out/bin/${prefix}dlltool
+      ln -s ${write-wrapper "lib"} $out/bin/${prefix}lib
+    '';
 
   # Compatibility packages here:
   propagatedBuildInputs = with targetPkgs; if targetPkgs == {} then [] else []
